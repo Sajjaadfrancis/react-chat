99ff3202a23ce4569341931893b34782bdef8156
unknown <sajjaadfrancis@gmail.com>
2020-07-13 20:24:58 +0200

    completed signup component

---
+++ src/dashboard/dashboard.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class DashboardComponent extends React.Component {
+  render() {
+    return <div>Hello world from dashboard</div>;
+  }
+}
+
+export default DashboardComponent;
