ecee890617e9ee144e5215d072df230a85fc7316
unknown <sajjaadfrancis@gmail.com>
2020-08-21 15:17:44 +0200

    deployment changes

---
+++ client/build/precache-manifest.6fa9109ebdd6098bfac41ce1ba8ea32e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "047e43b2cf0e9b7e8485c64214ba9491",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f5ef3653847e8390f62b",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2725e94de7dd47933fa7",
+    "url": "/static/js/2.8c6ed1d8.chunk.js"
+  },
+  {
+    "revision": "74c0ddca854ac003128884c6311253d0",
+    "url": "/static/js/2.8c6ed1d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5ef3653847e8390f62b",
+    "url": "/static/js/main.15ceb590.chunk.js"
+  },
+  {
+    "revision": "9bc2349bfbe76e87395f",
+    "url": "/static/js/runtime-main.e5afff51.js"
+  }
+]);
